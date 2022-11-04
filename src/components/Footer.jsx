a85fc3d9b5ede91552aa3890d52d73baef6d93cd
Judge-Paul <judgepaulogebe3@gmail.com>
2022-11-04 16:26:04 +0100

    add footer logo and contact button

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,18 +1,21 @@
 import React from "react"
 import I4G from "./images/I4G.png"
 import Zuri from "./images/zuri.jpg"
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
+import { FaGithub } from "react-icons/fa"
+import Slack from "./images/slack.png"
 
 export default function Footer() {
   return (
     <footer>
       <div className="top-row">
-        {/* <a href="https://github.com/Judge-Paul" target="_blank"><FontAwesomeIcon icon="coffee" /></a>
-        <a href="https://slack.com/Jadge" target="_blank"><FontAwesomeIcon icon="slack" /></a> */}
+        <a href="https://github.com/Judge-Paul" target="_blank"><FaGithub /></a>
+        <a href="https://slack.com/Jadge" target="_blank"><img src={Slack} alt="Slack" width="40px" /></a>
       </div>
       <div className="bot-row">
         <img src={Zuri} id="zuri"/>
-        <h3>HNG Internship 9 Frontend Task</h3>
+        <div className="HNG">
+          <h3>HNG Internship 9 Frontend Task</h3>
+        </div>
         <img src={I4G} id="i4g"/>
       </div>
     </footer>
--- src/components/Links.jsx
+++ src/components/Links.jsx
@@ -5,24 +5,25 @@ export default function Links() {
     let books = `http://books.zuri.team/python-for-begineers?ref_id=${slack}`
     return (
         <div className="links">
-        <a href="https://twitter.com/paulogebe" id="btn_zuri">Twitter Link</a>
-        <a href="https://training.zuri.team" id="btn_zuri">Zuri Team</a>
-        <a href={books} id="books"> Python Books
-            <div className= "subtext">
-                Get this beginner friendly book on Python
-            </div>
-        </a>
-        <a href="http://books.zuri.team" id="books"> Zuri Books
-            <div className= "subtext">
-                This is where you can find books about coding and design
-            </div>
-        </a>
-        <a href="https://background.zuri.team" id="pitch"> Background Check for Coders
-            <div className= "subtext">
-                Make background checks with ease
-            </div>
-        </a>
-        <a href="https://books.zuri.team/design-rules" id="book_design">Design Books</a>
+            <a href="https://twitter.com/paulogebe" id="btn_zuri">Twitter Link</a>
+            <a href="https://training.zuri.team" id="btn_zuri">Zuri Team</a>
+            <a href={books} id="books"> Python Books
+                <div className= "subtext">
+                    Get this beginner friendly book on Python
+                </div>
+            </a>
+            <a href="http://books.zuri.team" id="books"> Zuri Books
+                <div className= "subtext">
+                    This is where you can find books about coding and design
+                </div>
+            </a>
+            <a href="https://background.zuri.team" id="pitch"> Background Check for Coders
+                <div className= "subtext">
+                    Make background checks with ease
+                </div>
+            </a>
+            <a href="https://books.zuri.team/design-rules" id="book_design">Design Books</a>
+            <a href="#" id="contact">Contact Me</a>
         </div>
     )
 }
