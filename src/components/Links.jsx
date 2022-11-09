8c2aa762e098b1ea26622393287e58832854d364
Judge-Paul <judgepaulogebe3@gmail.com>
2022-11-09 20:24:31 +0100

    feat: show subtext on a hover
    added a feature to show the subtext or description of the buttons when you hover over them

--- src/components/Links.jsx
+++ src/components/Links.jsx
@@ -7,21 +7,18 @@ export default function Links() {
         <div className="links">
             <a href="https://twitter.com/paulogebe" id="btn_zuri">Twitter Link</a>
             <a href="https://training.zuri.team" id="btn_zuri">Zuri Team</a>
-            <a href={books} id="books"> Python Books
-                <div className= "subtext">
-                    Get this beginner friendly book on Python
-                </div>
-            </a>
-            <a href="http://books.zuri.team" id="books"> Zuri Books
-                <div className= "subtext">
-                    This is where you can find books about coding and design
-                </div>
-            </a>
-            <a href="https://background.zuri.team" id="pitch"> Background Check for Coders
-                <div className= "subtext">
-                    Make background checks with ease
-                </div>
-            </a>
+            <a href={books} id="books"> Python Books</a>
+            <div className= "subtext">
+                Get this beginner friendly book on Python
+            </div>
+            <a href="http://books.zuri.team" id="books"> Zuri Books</a>
+            <div className= "subtext">
+                This is where you can find books about coding and design
+            </div>
+            <a href="https://background.zuri.team" id="pitch"> Background Check for Coders</a>
+            <div className= "subtext">
+                Don't be deceived. Background checks on developers with ease.
+            </div>
             <a href="https://books.zuri.team/design-rules" id="book_design">Design Books</a>
             <a href="contact" id="contact">Contact Me</a>
         </div>
